cfca8877334bad333292fcfb1023c7d239fb8e59
xiehaijie <320974775@qq.com>
2023-05-26 16:59:19 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  env: { node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest' }
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  // 代码忽略分号
+  semi: false,
+  // 使用单引号代替双引号
+  singleQuote: true,
+  // 代码行超过 80 个字符会自动将代码折行
+  printWidth: 80,
+  // 在文本长度超过指定行宽时进行换行，否则保留文本的折行
+  proseWrap: 'always',
+  // tab 缩进为 2 个空格
+  tabWidth: 2,
+  // 项目中所使用的换行符类型为 \n
+  endOfLine: 'lf',
+  // 对象字面量中的花括号前后添加空格
+  bracketSpacing: true,
+  // 对象和数组字面量中不添加尾随逗号
+  trailingComma: 'none',
+  // 在箭头函数表达式有多个参数或参数需要使用括号包裹时，才需要添加括号
+  arrowParens: 'avoid',
+  // JSX 元素的起始标签和结束标签不处于同一行
+  jsxBracketSameLine: false
+}
